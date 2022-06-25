5398508acec5ca9bd0c277ae6ef4b6cfba59fc16
fumiyanll23 <fumiyanll23.public@gmail.com>
2022-06-25 22:14:34 +0900

    Add: exercises/chapter02/

---
+++ exercises/chapter02/ans.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Fibonacci数列の第n項を返す関数
+int fibonacci(int n) {
+  // ベースケース01
+  if (n == 1) {
+    return 1;
+  }
+  // ベースケース02
+  else if (n == 2) {
+    return 1;
+  }
+  // 再帰ステップ
+  else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+}
+
+// これ以降の行は変更しなくてよい
+
+int main() {
+  int n;
+  cin >> n;
+  cout << fibonacci(n) << endl;
+}
---
+++ exercises/chapter02/sample.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Fibonacci数列の第n項を返す関数
+int fibonacci(int n) {
+  // (ここに追記して再帰関数を実装する)
+}
+
+// これ以降の行は変更しなくてよい
+
+int main() {
+  int n;
+  cin >> n;
+  cout << fibonacci(n) << endl;
+}
